0972121b5b348c1c7f20513d8f07ec255e320944
Zain Saqib <xain.saqib@gmail.com>
2025-04-20 12:20:10 +0500

    Fixed minor bugs

--- TuringMacine.py
+++ TuringMacine.py
@@ -1,3 +1,13 @@
+
+def add(a,b):
+    return -1
+
+def multiply(a,b):
+    return -1
+
+
+
+
 def Subtract(a, b, show=True):
     tape = [1] * a + [-1] + [1] * b
     head = 0
@@ -38,7 +48,7 @@ def Divide(a, b):
     quotient = 0
 
     while a >= b:
-        a = Sub(a, b)
+        a = Subtract(a, b)
         quotient += 1
 
     print(f"Final Quotient: {quotient}")
@@ -50,12 +60,11 @@ def power(a,b):
         res=multiply(res,a)
         
     return res
-
 def main():
     print("unary turing machine for Arithmetic Operations: ")
     
     while True:
-        print("choose an option: ")
+        print("Choose an option: ")
         print("1: Add ")
         print("2: Subtract ")
         print("3: Multiply ")
@@ -64,39 +73,40 @@ def main():
         print("6: Exit ")
         
         choice = input("enter choice 1 to 6: ")
-        if choice=='6':
+        if choice == '6':
             print("exiting! ")
             break
         
         a = int(input("Enter your first number: "))
         b = int(input("Enter your second number: "))
         
-        if choice =='1':
-            add(a,b)
+        if choice == '1':
+            result = add(a, b)
+            print("Result:", result)
             
-        elif choice =='2':
-            if a<b:
-                printf("can not perforn subtract when a<b")
-                break
+        elif choice == '2':
+            if a < b:
+                print("can not perform subtract when a < b")
             else:
-                Subtract(a,b)
+                result = Subtract(a, b)
+                print("Result:", result)
         
-        elif choice =='3':
-            multiply(a,b)
+        elif choice == '3':
+            result = multiply(a, b)
+            print("Result:", result)
         
-        
-        elif choice =='4':
-            Divide(a,b)
+        elif choice == '4':
+            Divide(a, b)
             
-        
-        elif choice =='5':
-            power(a,b)
-        
+        elif choice == '5':
+            result = power(a, b)
+            print("Result:", result)
         
         else:
-            printf("invalid option selected!")
-            
-        
+            print("invalid option selected!")
 
 
 
+
+if __name__ == "__main__":
+    main()
